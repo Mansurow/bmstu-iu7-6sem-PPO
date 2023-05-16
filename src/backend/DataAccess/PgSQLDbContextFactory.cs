047376fe8da2c95760ee1da501a31697e7a5cde1
Mansurov Vladislav <reymax.vlad@gmail.com>
2023-05-16 18:38:27 +0300

    [lab-05] - fix

--- src/backend/DataAccess/PgSQLDbContextFactory.cs
+++ src/backend/DataAccess/PgSQLDbContextFactory.cs
@@ -14,7 +14,7 @@ namespace Anticafe.DataAccess
 
         public AppDbContext getDbContext()
         {
-            string curPerms = "default"; //_configuration["DbConnection"]!;
+            string curPerms = _configuration["DbConnection"]!;
 
             var builder = new DbContextOptionsBuilder<AppDbContext>();
             builder.UseNpgsql(_configuration.GetConnectionString(curPerms));
