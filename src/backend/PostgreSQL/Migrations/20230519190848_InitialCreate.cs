49b4725147ee99c0ad59d5cdc5acf9be50932aa5
Mansurov Vladislav <reymax.vlad@gmail.com>
2023-05-22 20:29:23 +0300

    [lab-09] fix PostgreSQL migration

---
+++ src/backend/PostgreSQL/Migrations/20230519190848_InitialCreate.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(64)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(64)", nullable: false),
+                    type = table.Column<string>(type: "varchar(64)", nullable: false),
+                    price = table.Column<double>(type: "double precision", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(64)", nullable: false),
+                    size = table.Column<int>(type: "integer", nullable: false),
+                    price = table.Column<double>(type: "double precision", nullable: false),
+                    Raiting = table.Column<double>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    surname = table.Column<string>(type: "varchar(64)", nullable: false),
+                    name = table.Column<string>(type: "varchar(64)", nullable: false),
+                    first_name = table.Column<string>(type: "varchar(64)", nullable: false),
+                    birthday = table.Column<string>(type: "varchar(64)", nullable: false),
+                    gender = table.Column<string>(type: "varchar(64)", nullable: false),
+                    email = table.Column<string>(type: "varchar(64)", nullable: false),
+                    phone = table.Column<string>(type: "varchar(64)", nullable: false),
+                    password = table.Column<string>(type: "varchar(128)", nullable: true),
+                    role = table.Column<string>(type: "varchar(64)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryDbModelRoomDbModel",
+                columns: table => new
+                {
+                    InventoriesId = table.Column<int>(type: "integer", nullable: false),
+                    RoomsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryDbModelRoomDbModel", x => new { x.InventoriesId, x.RoomsId });
+                    table.ForeignKey(
+                        name: "FK_InventoryDbModelRoomDbModel_Inventories_InventoriesId",
+                        column: x => x.InventoriesId,
+                        principalTable: "Inventories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InventoryDbModelRoomDbModel_Rooms_RoomsId",
+                        column: x => x.RoomsId,
+                        principalTable: "Rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    room_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    amount_of_people = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    start_time = table.Column<string>(type: "varchar(64)", nullable: false),
+                    end_time = table.Column<string>(type: "varchar(64)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Rooms_room_id",
+                        column: x => x.room_id,
+                        principalTable: "Rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    room_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<string>(type: "varchar(64)", nullable: false),
+                    mark = table.Column<int>(type: "integer", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Feedbacks_Rooms_room_id",
+                        column: x => x.room_id,
+                        principalTable: "Rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Feedbacks_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_room_id",
+                table: "Bookings",
+                column: "room_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_user_id",
+                table: "Bookings",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_room_id",
+                table: "Feedbacks",
+                column: "room_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_user_id",
+                table: "Feedbacks",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryDbModelRoomDbModel_RoomsId",
+                table: "InventoryDbModelRoomDbModel",
+                column: "RoomsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.DropTable(
+                name: "InventoryDbModelRoomDbModel");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
