6220e7eb2b03e0528769c1ddb4c974023e5b60b4
Mansurov Vladislav <reymax.vlad@gmail.com>
2023-05-31 11:51:40 +0300

    [lab-07] fix

--- src/frotend/src/services/http-service/user.service.tsx
+++ src/frotend/src/services/http-service/user.service.tsx
@@ -9,40 +9,11 @@ import { UserFormProps } from "../../models/types";
 export default class UserService {
     static api = '/auth';
 
-    static MockUsers: User[] =[
-        {
-            id: 1,
-            surname: "Ivan",
-            name: "Ivan",
-            firstname: "ivan",
-            gender: Gender.Male,
-            birthday: "17-03-2002",
-            email: "user1",
-            password: "password",
-            role: UserType.User,
-            phone: "12121"
-        },
-        {
-            id: 2,
-            surname: "Ivan",
-            name: "Ivan",
-            firstname: "ivan",
-            gender: Gender.Male,
-            birthday: "17-03-2002",
-            email: "admin",
-            password: "admin",
-            role: UserType.Admin,
-            phone: "12121"
-        }
-    ]
-
     static async SignIn(user: UserFormProps): Promise<User | undefined> {
-        //return await axios.get(ResourseService.url + this.api + `/${user.login}&${user.password}`);
-        return this.MockUsers.find(u => u.email === user.login && u.password === user.password);
+        return await axios.get(ResourseService.url + this.api + `/${user.login}&${user.password}`);
     }
 
     static async SignUp(user: UserFormProps): Promise<User> {
         return await axios.get(ResourseService.url + this.api + `/${user.login}&${user.password}`);
-        // return this.MockUsers[0];
     }
 }
