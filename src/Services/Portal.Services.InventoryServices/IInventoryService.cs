c2a3e2519c03c88994c97844666833ce40ee8350
Mansurov Vladislav <reymax.vlad@gmail.com>
2023-07-22 22:08:49 +0300

    [lab_03] add InventoryService

---
+++ src/Services/Portal.Services.InventoryServices/IInventoryService.cs
@@ -0,0 +1,45 @@
+﻿using Portal.Common.Models;
+
+namespace Portal.Services.InventoryServices;
+
+/// <summary>
+/// Интерфейс сервиса инвентаря
+/// </summary>
+public interface IInventoryService
+{
+    /// <summary>
+    /// Получить весь инвентарь
+    /// </summary>
+    /// <returns>Список всего инвентаря</returns>
+    Task<List<Inventory>> GetAllInventoriesAsync();
+
+    /// <summary>
+    /// Получить инчентаря
+    /// </summary>
+    /// <param name="inventoryId">Идентификатор инвентаря</param>
+    /// <returns>Данные инвентаря</returns>
+    Task<Inventory> GetInventoryByIdAsync(Guid inventoryId);
+
+    /// <summary>
+    /// Добавить инвентарь
+    /// </summary>
+    /// <param name="name">Название</param>
+    /// <param name="yearOfProduction">Год выпуска или производства</param>
+    /// <param name="description">Описание инветаря</param>
+    /// <returns>Идентификатор нового инветаря</returns>
+    Task<Guid> AddInventoryAsync(string name, DateOnly yearOfProduction, string description);
+
+    /// <summary>
+    /// Обновить инвентарь
+    /// </summary>
+    /// <param name="inventory">Данные инвентаря на обновление</param>
+    /// <returns></returns>
+    Task UpdateInventoryAsync(Inventory inventory);
+    
+    /// <summary>
+    /// Удалить инвентарь
+    /// </summary>
+    /// <param name="inventoryId">Идентификатор инвентаря</param>
+    /// <returns></returns>
+    Task RemoveInventoryAsync(Guid inventoryId);
+}
